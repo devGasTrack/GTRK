549dae4749851d24ae66891d3009c5b5b548986d
Fayele Moses <fayolumoses@gmail.com>
2024-08-27 16:12:44 +0100

    Build Script

---
+++ build/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define directories
+SRC_DIR="../Firmware/src"
+INC_DIR="../include"
+BUILD_DIR="../build/build_files"
+
+# Create build directory if it doesn't exist
+mkdir -p "$BUILD_DIR"
+
+# Compile source files into object files
+sdcc -mmcs51 -c -I"$INC_DIR" "$SRC_DIR/main.c" -o "$BUILD_DIR/main.rel"
+sdcc -mmcs51 -c -I"$INC_DIR" "$SRC_DIR/utils.c" -o "$BUILD_DIR/utils.rel"
+
+# Link object files
+sdcc -mmcs51 "$BUILD_DIR/main.rel" "$BUILD_DIR/utils.rel" -o "$BUILD_DIR/main.ihx"
+
+# Convert to HEX file
+packihx "$BUILD_DIR/main.ihx" > "$BUILD_DIR/main.hex"
+
+echo "Build completed successfully!"
---
+++ build/build_files/utils.asm
@@ -0,0 +1,144 @@
+;--------------------------------------------------------
+; File Created by SDCC : free open source ISO C Compiler 
+; Version 4.4.0 #14620 (Mac OS X x86_64)
+;--------------------------------------------------------
+	.module utils
+	.optsdcc -mmcs51 --model-small
+	
+;--------------------------------------------------------
+; Public variables in this module
+;--------------------------------------------------------
+	.globl _delay
+;--------------------------------------------------------
+; special function registers
+;--------------------------------------------------------
+	.area RSEG    (ABS,DATA)
+	.org 0x0000
+;--------------------------------------------------------
+; special function bits
+;--------------------------------------------------------
+	.area RSEG    (ABS,DATA)
+	.org 0x0000
+;--------------------------------------------------------
+; overlayable register banks
+;--------------------------------------------------------
+	.area REG_BANK_0	(REL,OVR,DATA)
+	.ds 8
+;--------------------------------------------------------
+; internal ram data
+;--------------------------------------------------------
+	.area DSEG    (DATA)
+;--------------------------------------------------------
+; overlayable items in internal ram
+;--------------------------------------------------------
+	.area	OSEG    (OVR,DATA)
+;--------------------------------------------------------
+; indirectly addressable internal ram data
+;--------------------------------------------------------
+	.area ISEG    (DATA)
+;--------------------------------------------------------
+; absolute internal ram data
+;--------------------------------------------------------
+	.area IABS    (ABS,DATA)
+	.area IABS    (ABS,DATA)
+;--------------------------------------------------------
+; bit data
+;--------------------------------------------------------
+	.area BSEG    (BIT)
+;--------------------------------------------------------
+; paged external ram data
+;--------------------------------------------------------
+	.area PSEG    (PAG,XDATA)
+;--------------------------------------------------------
+; uninitialized external ram data
+;--------------------------------------------------------
+	.area XSEG    (XDATA)
+;--------------------------------------------------------
+; absolute external ram data
+;--------------------------------------------------------
+	.area XABS    (ABS,XDATA)
+;--------------------------------------------------------
+; initialized external ram data
+;--------------------------------------------------------
+	.area XISEG   (XDATA)
+	.area HOME    (CODE)
+	.area GSINIT0 (CODE)
+	.area GSINIT1 (CODE)
+	.area GSINIT2 (CODE)
+	.area GSINIT3 (CODE)
+	.area GSINIT4 (CODE)
+	.area GSINIT5 (CODE)
+	.area GSINIT  (CODE)
+	.area GSFINAL (CODE)
+	.area CSEG    (CODE)
+;--------------------------------------------------------
+; global & static initialisations
+;--------------------------------------------------------
+	.area HOME    (CODE)
+	.area GSINIT  (CODE)
+	.area GSFINAL (CODE)
+	.area GSINIT  (CODE)
+;--------------------------------------------------------
+; Home
+;--------------------------------------------------------
+	.area HOME    (CODE)
+	.area HOME    (CODE)
+;--------------------------------------------------------
+; code
+;--------------------------------------------------------
+	.area CSEG    (CODE)
+;------------------------------------------------------------
+;Allocation info for local variables in function 'delay'
+;------------------------------------------------------------
+;ms                        Allocated to registers r6 r7 
+;i                         Allocated to registers r4 r5 
+;j                         Allocated to registers r2 r3 
+;------------------------------------------------------------
+;	../Firmware/src/utils.c:3: void delay(unsigned int ms) {
+;	-----------------------------------------
+;	 function delay
+;	-----------------------------------------
+_delay:
+	ar7 = 0x07
+	ar6 = 0x06
+	ar5 = 0x05
+	ar4 = 0x04
+	ar3 = 0x03
+	ar2 = 0x02
+	ar1 = 0x01
+	ar0 = 0x00
+	mov	r6, dpl
+	mov	r7, dph
+;	../Firmware/src/utils.c:5: for (i = 0; i < ms; i++) {
+	mov	r4,#0x00
+	mov	r5,#0x00
+00107$:
+	clr	c
+	mov	a,r4
+	subb	a,r6
+	mov	a,r5
+	subb	a,r7
+	jnc	00109$
+;	../Firmware/src/utils.c:6: for (j = 0; j < 123; j++) {
+	mov	r2,#0x7b
+	mov	r3,#0x00
+00105$:
+	dec	r2
+	cjne	r2,#0xff,00138$
+	dec	r3
+00138$:
+	mov	a,r2
+	orl	a,r3
+	jnz	00105$
+;	../Firmware/src/utils.c:5: for (i = 0; i < ms; i++) {
+	inc	r4
+	cjne	r4,#0x00,00107$
+	inc	r5
+	sjmp	00107$
+00109$:
+;	../Firmware/src/utils.c:9: }
+	ret
+	.area CSEG    (CODE)
+	.area CONST   (CODE)
+	.area XINIT   (CODE)
+	.area CABS    (ABS,CODE)
