1fad1ce2bb4015583397124a7f99cadda7036394
Fayele Moses <fayolumoses@gmail.com>
2024-09-01 01:45:36 +0100

    Uart 0 Interrupt fix

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -9,10 +9,15 @@ void timer0_isr(void) __interrupt(INT_NO_TMR0){
 }
 
 void uart0_isr(void) __interrupt(INT_NO_UART0){
-    if(uart_counter <= 255)
-        uart_buf[uart_counter++] = SBUF;
-        
-    SCON &= ~0x02;
+    if (SCON & 0x01) { 
+        if(uart_counter <= 255)
+            uart_buf[uart_counter++] = SBUF;
+
+        SCON &= ~0x02;
+    }
+    if (SCON & 0x02) { 
+        SCON &= ~0x02;
+    }
 }
 
 void main(void) {
